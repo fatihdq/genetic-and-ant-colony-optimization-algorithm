f4e3f326d371db28048845912c18db9381487a04
fatih.dq19 <samgong23@gmail.com>
2024-07-15 19:44:41 +0700

    add new chart 3 line

--- main_testcase.py
+++ main_testcase.py
@@ -45,6 +45,10 @@ testcase['Image GA-ACO 2 Line Percobaan 3'] = testcase['Image GA-ACO 2 Line Perc
 testcase['Image GA-ACO 1 Line Percobaan 1'] = testcase['Image GA-ACO 1 Line Percobaan 1'].astype(str)
 testcase['Image GA-ACO 1 Line Percobaan 2'] = testcase['Image GA-ACO 1 Line Percobaan 2'].astype(str)
 testcase['Image GA-ACO 1 Line Percobaan 3'] = testcase['Image GA-ACO 1 Line Percobaan 3'].astype(str)
+testcase['Image GA-ACO All Percobaan 1'] = testcase['Image GA-ACO All Percobaan 1'].astype(str)
+testcase['Image GA-ACO All Percobaan 2'] = testcase['Image GA-ACO All Percobaan 2'].astype(str)
+testcase['Image GA-ACO All Percobaan 3'] = testcase['Image GA-ACO All Percobaan 3'].astype(str)
+
 
 
 
@@ -81,6 +85,7 @@ if __name__ == '__main__':
             GAACOACOImageFilename = './tcImages/'+dataset.replace('.csv','')+'_'+str(testIdx)+'_GA_ACO_(ACO)_'+str(generation)+'.png'
             GAACO2ImageFilename = './tcImages/'+dataset.replace('.csv','')+'_'+str(testIdx)+'_GA_ACO_(2 line)_'+str(generation)+'.png'
             GAACO1ImageFilename = './tcImages/'+dataset.replace('.csv','')+'_'+str(testIdx)+'_GA_ACO_(1 line)_'+str(generation)+'.png'
+            GAACOAllImageFilename = './tcImages/'+dataset.replace('.csv','')+'_'+str(testIdx)+'_GA_ACO_All_'+str(generation)+'.png'
 
             start_time = time.time()
             # Start Genetic Algorithm process
@@ -116,6 +121,7 @@ if __name__ == '__main__':
             start_time = time.time()
             # Start Ant Colony Optimization Algorithm process
             acoDistance, acoProgress = antColonyOptimization(city=city, iteration=iteration, nAnts=nAnts, rho=rho, alpha=alpha, beta=beta, initialPheromne=initialPheromne, DEBUG=DEBUG)
+            acoProgress = [item[0] for item in acoProgress]
             aco_time = time.time() - start_time
 
             # Visualize the Ant Colony Optimization Algorithm result
@@ -131,22 +137,30 @@ if __name__ == '__main__':
             start_time = time.time()
             # Start Genetic Algorithm & Ant Colony Optimization Algorithm process
             gaAcoDistance, gaAcoProgress = gaAcoAlgorithm(city=city, iteration=iteration, nAnts=nAnts, rho=rho, alpha=alpha, beta=beta, initialPheromne=initialPheromne, routes=newPop, DEBUG=DEBUG)
+            gaAcoProgress = [item[0] for item in gaAcoProgress]
             ga_aco_time = time.time() - start_time
 
             # Visualize the Genetic Algorithm & Ant Colony Optimization Algorithm result
-            plt.figure(idx+testIdx+3)
-            plt.plot(range(1, generation + 1), gaProgress, label = 'GA')
-            plt.plot(range(1, iteration + 1), gaAcoProgress, label = 'ACO')
-            plt.legend()
+            fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 2]})
+            ax1.plot(range(1, generation + 1), gaProgress, color= '#d16747' , label = 'GA')
+            ax2.plot(range(1, iteration + 1), gaAcoProgress, color= '#383eb0', label = 'GA-ACO')
+            ax1.set_ylim(min(gaProgress) - (min(gaProgress)*0.001), max(gaProgress) + (max(gaProgress)*0.001))
+            ax2.set_ylim(min(gaAcoProgress) - (min(gaAcoProgress)*0.001), max(gaAcoProgress) + (max(gaAcoProgress)*0.001))
+            ax1.spines.bottom.set_visible(False)
+            ax2.spines.top.set_visible(False)
+            ax1.xaxis.tick_top()
+            ax1.tick_params(labeltop=False)
+            ax2.xaxis.tick_bottom()
+            fig.legend()
+            fig.suptitle('GA & ACO Result')
             plt.ylabel('Distance')
             plt.xlabel('Generation')
-            plt.title('GA & ACO Result')
             plt.savefig(GAACO2ImageFilename)
             plt.close()
 
             # Visualize the Genetic Algorithm & Ant Colony Optimization (ACO Only) Algorithm result
             plt.figure(idx+testIdx+4)
-            plt.plot(range(1, iteration + 1), gaAcoProgress, label = 'ACO')
+            plt.plot(range(1, iteration + 1), gaAcoProgress, label = 'GA-ACO (ACO)')
             plt.legend()
             plt.ylabel('Distance')
             plt.xlabel('Generation')
@@ -156,13 +170,34 @@ if __name__ == '__main__':
 
             # Visualize the Genetic Algorithm & Ant Colony Optimization 1 line Algorithm result            
             plt.figure(idx+testIdx+5)
-            plt.plot(gaProgress + [item[0] for item in gaAcoProgress], label = 'ACO')
+            plt.plot(gaProgress + gaAcoProgress, label = 'GA-ACO')
             plt.legend()
             plt.ylabel('Distance')
             plt.xlabel('Generation, GA: '+str(generation)+' (1-'+ str(generation) +'), ACO: '+str(iteration)+' ('+str(generation+1)+'-'+str(generation+iteration)+')')
             plt.title('GA & ACO Result')
             plt.savefig(GAACO1ImageFilename)
             plt.close()
+
+            # Visualize the Genetic Algorithm & Ant Colony Optimization 1 line Algorithm result            
+            fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 2]})
+            ax1.plot(range(1, generation + 1), gaProgress, color= '#d16747' , label = 'GA')
+            ax2.plot(range(1, iteration + 1), acoProgress, color= '#6cb038', label = 'ACO')
+            ax2.plot(range(1, iteration + 1), gaAcoProgress, color= '#383eb0', label = 'GA-ACO')
+            appendedProgress = acoProgress+gaAcoProgress
+            ax1.set_ylim(min(gaProgress) - (min(gaProgress)*0.001), max(gaProgress) + (max(gaProgress)*0.001))
+            ax2.set_ylim(min(appendedProgress) - (min(appendedProgress)*0.001), max(appendedProgress) + (max(appendedProgress)*0.001))
+            ax1.spines.bottom.set_visible(False)
+            ax2.spines.top.set_visible(False)
+            ax1.xaxis.tick_top()
+            ax1.tick_params(labeltop=False)
+            ax2.xaxis.tick_bottom()
+            fig.legend()
+            fig.suptitle('GA, ACO, & GA-ACO Result')
+            plt.ylabel('Distance')
+            plt.xlabel('Generation')
+            plt.savefig(GAACOAllImageFilename)
+            plt.close()
+
 
             testcase.loc[idx, "Jarak GA Percobaan " + str(testIdx)] = gaDistance
             testcase.loc[idx, "Jarak ACO Percobaan " + str(testIdx)] = acoDistance
@@ -177,6 +212,7 @@ if __name__ == '__main__':
             testcase.loc[idx, "Image GA-ACO Percobaan " + str(testIdx)] = GAACOACOImageFilename
             testcase.loc[idx, "Image GA-ACO 2 Line Percobaan " + str(testIdx)] = GAACO2ImageFilename
             testcase.loc[idx, "Image GA-ACO 1 Line Percobaan " + str(testIdx)] = GAACO1ImageFilename
+            testcase.loc[idx, "Image GA-ACO All Percobaan " + str(testIdx)] = GAACOAllImageFilename
         
         avgDistanceGA = (testcase.loc[idx, "Jarak GA Percobaan 1"] + testcase.loc[idx, "Jarak GA Percobaan 2"] + testcase.loc[idx, "Jarak GA Percobaan 3"])/3
         avgDistanceACO = (testcase.loc[idx, "Jarak ACO Percobaan 1"] + testcase.loc[idx, "Jarak ACO Percobaan 2"] + testcase.loc[idx, "Jarak ACO Percobaan 3"])/3
